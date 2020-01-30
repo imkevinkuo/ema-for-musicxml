97135cf19cc524d71115e200347daaa2a4e5ea30
Kevin Kuo <kvkkuo@gmail.com>
2020-01-30 12:45:45 -0500

    Copies over part info, can handle no-comma expressions

--- ema2/slicer.py
+++ ema2/slicer.py
@@ -45,15 +45,54 @@ def slice_score(score: stream.Score, ema_exp: EmaExpression):
     score_info = get_score_info(score)
     measure_nums = ema_to_list(ema_exp.mm_ranges, score_info, 'measure')
     new_score = stream.Score()
+    # music21 streams are organized by Score > Part > Measure
+    # Create empty parts inside the new score, copy other objects
+    for item in score:
+        if type(item) == stream.Part:
+            new_part = stream.Part()
+            new_part.partName = item.partName
+            new_part.partAbbreviation = item.partAbbreviation
+            new_part.id = item.id
+            for part_item in item:
+                if type(part_item) != stream.Measure:
+                    new_part.append(part_item)
+            new_score.append(new_part)
+        else:
+            new_score.append(item)
     for m in range(len(measure_nums)):
         measure_num = measure_nums[m]
-        measure_staves = ema_to_list(ema_exp.st_ranges[m], score_info, 'staff')
+
+        # this is hacky ill fix later
+        m2 = m
+        if len(ema_exp.st_ranges) == 1:
+            m2 = 0
+        measure_staves = ema_to_list(ema_exp.st_ranges[m2], score_info, 'staff')
+
+        # Create blank measure if part was not selected for this measure
+        for s in range(score_info['staff']['end']):
+            if s+1 not in measure_staves:
+                new_score.parts[s].append(stream.Measure(measure_num))
+        # Insert beats for parts that were requested
         for s in range(len(measure_staves)):
             stave_num = measure_staves[s]
-            staff_beats = ema_to_list(ema_exp.bt_ranges[m][s], score_info, 'beat', measure_num)
+            print(m, s)
+            # this is hacky too ill fix later
+            s2 = s
+            if len(ema_exp.bt_ranges) == 1:
+                s2 = 0
+            staff_beats = ema_to_list(ema_exp.bt_ranges[m2][s2], score_info, 'beat', measure_num)
+
             # Printout of all requested beats, with measure and stave matched up
-            print(measure_num, stave_num, staff_beats)
-            # new_score.append(score.measure(measure_num).parts[stave_num])
+            # print(measure_num, stave_num, m, s, staff_beats)
+            # this is gonna be complicated, maybe have to iterate from time 0 up to end
+            # for b in staff_beats:
+            # gonna have to make a function to get X note at Y time step
+            new_measure = stream.Measure(measure_num)
+            notes = score.parts[stave_num - 1].measure(measure_num).notesAndRests
+            for note in notes:
+                new_measure.append(note)
+            new_score.parts[stave_num - 1].append(new_measure)
+    return new_score
     # try:
     #     file_path = new_score.write('musicxml')
     # except Exception as e:
--- test.py
+++ test.py
@@ -1,13 +1,14 @@
-path = "tests/omen.mxl"
+import ema2.slicer as slicer
+path = "tests/omen_small.mxl"
 from music21 import stream, converter
 from ema2 import emaexpression
 score: stream.Score = converter.parse(path)
-measures = "1-3,4,5-6"
-staves = "1,2,3,1-3+5,2+3,4"
+measures = "all"
+# measures = "1-3,4,5-6"
+staves = "all"
+# staves = "1,2,3,1-3+5,2+3,4"
 beats = "@all"
-beats = "@all,@all,@all,@1-2@3+@all+@all+@all,@all+@all,@all"
+# beats = "@all,@all,@all,@1-2@3+@all+@all+@all,@all+@all,@all"
 completeness = None
 ema_exp = emaexpression.EmaExpression(measures, staves, beats, completeness)
-from ema2.slicer import get_score_info, ema_to_list
-score_info = get_score_info(score)
-measure_nums = ema_to_list(ema_exp.mm_ranges, score_info, 'measure')
+new_score = slicer.slice_score(score, ema_exp)
