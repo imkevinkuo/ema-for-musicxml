73934f79052eb58d26d084996bab1e084514b4f0
Kevin Kuo <kvkkuo@gmail.com>
2020-02-03 15:58:18 -0500

    EmaExp expansion works when using music21 to find start/end values

--- test.py
+++ test.py
@@ -1,13 +1,14 @@
-import ema2.slicer as slicer
-path = "tests/omen_small.mxl"
 from music21 import stream, converter
-from ema2 import emaexpression
+from ema2 import emaexpression, emaexpressionfull
+path = "tests/omen_small.musicxml"
 score: stream.Score = converter.parse(path)
-measures = "all"
-# measures = "1-3,4,5-6"
-staves = "all"
-# staves = "1,2,3,1-3+5,2+3,4"
-beats = "@all"
-# beats = "@all,@all,@all,@1-2@3+@all+@all+@all,@all+@all,@all"
+# measures = "all"
+# staves = "all"
+# beats = "@all"
+measures = "1-3,4,5-6"
+staves = "1,2,3,1-3+5,2+3,4"
+beats = "@all,@all,@all,@1-2@3+@all+@all+@all,@all+@all,@all"
 completeness = None
 ema_exp = emaexpression.EmaExpression(measures, staves, beats, completeness)
+score_info = emaexpressionfull.get_score_info_m21(score)
+ema_exp_full = emaexpressionfull.EmaExpressionFull(score_info, ema_exp)
