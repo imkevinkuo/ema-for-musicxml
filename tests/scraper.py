d98f75c12c54f605f5cee9e85bea79e903bd681f
imkevinkuo <kvkkuo@gmail.com>
2020-03-03 12:05:37 -0500

    Code to download all Digital DuChemin scores

--- tests/scraper.py
+++ tests/scraper.py
@@ -1,5 +1,7 @@
 import requests
 from music21 import converter, stream, environment
+from tqdm import tqdm
+
 from ema2.emaexp import EmaExp
 from bs4 import BeautifulSoup
 from pyld import jsonld
@@ -14,6 +16,7 @@ LAST_PAGE = 11
 
 
 def get_jsonlds(page_num):
+    """ Fetches a list of .jsonld file URLs for each nanopub on the specified page. """
     r = requests.get(f"{NANOPUB_URL}/nanopubs.html?page={page_num}")
     soup = BeautifulSoup(r.text, 'html.parser')
     results = soup.findAll("a", text="jsonld", attrs={"type": "application/ld+json"})
@@ -22,6 +25,7 @@ def get_jsonlds(page_num):
 
 
 def ema_url_from_jsonld(jsonld_filename):
+    """ Takes a .jsonld filename and extracts the full EMA request URL. """
     nanopub = jsonld.load_document(f"{NANOPUB_URL}/{jsonld_filename}")
     for graph in nanopub["document"]:
         for item in graph["@graph"]:
@@ -31,11 +35,13 @@ def ema_url_from_jsonld(jsonld_filename):
 
 
 def ema_exp_from_jsonld(jsonld_filename):
+    """ Gets the EMA URL from a jsonld, then constructs an EmaExp from the URL. """
     ema_url = ema_url_from_jsonld(jsonld_filename)
     return EmaExp(*ema_url.split("/")[:-3])
 
 
 def ema_exps_from_page(page_num):
+    """ Constructs an EmaExp for every nanopub on the page. """
     file_names = get_jsonlds(page_num)
     ema_exps = []
     for file_name in file_names:
@@ -43,15 +49,14 @@ def ema_exps_from_page(page_num):
     return ema_exps
 
 
-def get_omas_selections(page_num):
-    """ Construct a dictionary[score_name][ema_string] = selection xml based on Omas nanopubs. """
+def add_omas_sel_truth_to_dict(d, page_num):
+    """ Add entries to a dictionary[score_name][ema_string] = selection xml for each nanopub on the specified page. """
     jsonlds = get_jsonlds(page_num)
     ema_urls = [ema_url_from_jsonld(j) for j in jsonlds]
     mei_urls = [unquote(ema_url.split("/")[-4]) for ema_url in ema_urls]
+    fails = []
 
-    selections_truth = {}
-
-    for i in range(len(mei_urls)):
+    for i in tqdm(range(len(mei_urls))):
         mei_url = mei_urls[i]
         ema_url = ema_urls[i]
 
@@ -59,21 +64,27 @@ def get_omas_selections(page_num):
         name_ext = filename.split(".")
         expr = ema_url.split("/")[-3:]
 
-        # Downloads the MEI score, converts to XML, saves as a file. Use etree to load from file.
-        if not os.path.exists(f"data/{filename}"):
-            score = converter.parseURL(mei_url, format='mei')
-            score.write("musicxml", fp=f"data/{filename}")
+        # Can't suppress "mei.base: WARNING: Importing <slur> without @startid and @endid is not yet supported."
+        try:
+            # Downloads the MEI score, converts to XML, saves as a file. Use etree to load from file.
+            if not os.path.exists(f"data/{filename}"):
+                score = converter.parseURL(mei_url, format='mei')
+                score.write("musicxml", fp=f"data/{filename}")
 
-        # Saves the MEI selection into a temp file and loads it back into memory.
-        selection_score = converter.parseURL(ema_url, format='mei')
-        selection_path = selection_score.write("musicxml", fp=f"data/temp_selection.xml")
-        omas_tree = ET.parse(selection_path)
+            # Saves the MEI selection into a temp file and loads it back into memory.
+            selection_score = converter.parseURL(ema_url, format='mei')
+            selection_path = selection_score.write("musicxml", fp=f"data/temp_selection.xml")
+            omas_tree = ET.parse(selection_path)
 
-        if name_ext[0] not in selections_truth:
-            selections_truth[name_ext[0]] = {}
-        selections_truth[name_ext[0]]["/".join(expr)] = omas_tree # Maybe ET.tostring
+            if name_ext[0] not in d:
+                d[name_ext[0]] = {}
+            d[name_ext[0]]["/".join(expr)] = omas_tree # Maybe ET.tostring
+        # except converter.ConverterException:
+        except Exception as ex:
+            fails.append((i,ex))
+            # print(f"{i}: {ex]")
 
-    return selections_truth
+    return d, fails
 
 
 def ema2_vs_omas(selections_truth, score_name, expr_str):
@@ -88,6 +99,15 @@ def ema2_vs_omas(selections_truth, score_name, expr_str):
     ema_exp_full = emaexpfull.EmaExpFull(score_info, ema_exp)
     ema2_tree = slicer.slice_score(tree, ema_exp_full)
     # Make a diff report - compare to selections_truth[score_name][expr]
+
+
+def scrape_all_scores():
+    d = {}
+    fails = []
+    for i in range(1, LAST_PAGE + 1):
+        d2, fails2 = add_omas_sel_truth_to_dict(d, i)
+        fails += fails2
+    return d, fails
 
 
 def get_omas_and_ema2_trees(npub_num):
